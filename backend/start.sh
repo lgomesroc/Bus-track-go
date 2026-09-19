790eaa63ec5cd49f977b7bedb3164e655c8d02b1
lgomesroc <lgomesroc2012@gmail.com>
2026-09-19 16:47:39 -0300

    support Oracle wallet secret on Render

--- backend/start.sh
+++ backend/start.sh
@@ -3,8 +3,9 @@ set -e
 
 mkdir -p /tmp/oracle-wallet
 
-if [ -f /etc/secrets/bustrack-wallet.zip ]; then
-    unzip -q /etc/secrets/bustrack-wallet.zip -d /tmp/oracle-wallet
+if [ -f /etc/secrets/bustrack-wallet.zip.b64 ]; then
+    base64 -d /etc/secrets/bustrack-wallet.zip.b64 > /tmp/bustrack-wallet.zip
+    unzip -q /tmp/bustrack-wallet.zip -d /tmp/oracle-wallet
     export TNS_ADMIN=/tmp/oracle-wallet/bustrack
 fi
 
