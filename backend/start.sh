c66dade7ea03a6c219c0743e0c75c09971d25919
lgomesroc <lgomesroc2012@gmail.com>
2026-09-19 17:40:52 -0300

    fix: configure wallet path for render

--- backend/start.sh
+++ backend/start.sh
@@ -7,6 +7,12 @@ mkdir -p /tmp/oracle-wallet
 if [ -f /etc/secrets/bustrack-wallet.zip.b64 ]; then
     base64 -d /etc/secrets/bustrack-wallet.zip.b64 > /tmp/bustrack-wallet.zip
     unzip -q /tmp/bustrack-wallet.zip -d /tmp/oracle-wallet
+
+    cat > /tmp/oracle-wallet/bustrack/sqlnet.ora <<'EOF'
+WALLET_LOCATION = (SOURCE = (METHOD = file) (METHOD_DATA = (DIRECTORY="/tmp/oracle-wallet/bustrack")))
+SSL_SERVER_DN_MATCH=yes
+EOF
+
     export TNS_ADMIN=/tmp/oracle-wallet/bustrack
 fi
 
